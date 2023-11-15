3ad71efbe4029f591d2bd5da062a9936f78848bd
Tulza <40633228+tulza@users.noreply.github.com>
2023-11-15 22:23:50 +1300

    testing no.3

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/news-homepage-main/",
 
   scripts: {
     build: "vite build",
     preview: "vite preview",
   },
 
-  base: "/news-homepage-main/",
 });
